cc7e3f0c0add57532109503babb8a124586cedd5
paliwalarpan <arpan.paliwal12@gmail.com>
2022-11-15 19:55:18 +0530

    Initializer commit for cache sample

---
+++ src/main/java/com/scaler/springboot/cachingsample/CachingsampleApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.springboot.cachingsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/springboot/cachingsample/CachingsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.springboot.cachingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
