73e966893e1ef069257e402bffe3362e2a178d9d
paliwalarpan <arpan.paliwal12@gmail.com>
2022-11-17 10:21:13 +0530

    Spring boot cachable caffine integration

--- src/test/java/com/scaler/springboot/cachingsample/MathServiceTest.java
+++ src/test/java/com/scaler/springboot/cachingsample/MathServiceTest.java
@@ -1,18 +1,24 @@
 package com.scaler.springboot.cachingsample;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {MathServiceTest.Config.class})
@@ -20,6 +26,9 @@ class MathServiceTest {
 
     @SpyBean
     private MathService mathService;
+
+    @Autowired
+    private CacheManager cacheManager;
 
     @Configuration
     @EnableCaching
@@ -30,17 +39,42 @@ class MathServiceTest {
         }
 
         @Bean
-        CacheManager cacheManager() {
-            return new ConcurrentMapCacheManager();
+        public CacheManager cacheManager() {
+            CaffeineCacheManager cacheManager = new CaffeineCacheManager("factorial");
+            cacheManager.setCaffeine(caffeineCacheBuilder());
+            return cacheManager;
+        }
+
+        Caffeine<Object, Object> caffeineCacheBuilder() {
+            return Caffeine.newBuilder()
+                    .initialCapacity(100)
+                    .maximumSize(100)
+                    .expireAfterAccess(10, TimeUnit.SECONDS)
+                    .recordStats();
         }
     }
 
     @Test
-    public void testCacheBehaviour() {
+    void testCacheBehaviour() {
         int result = mathService.factorialOf(11);
         int resultFromCache = mathService.factorialOf(11);
         Mockito.verify(mathService, Mockito.times(1)).factorialOf(11);
-        assertEquals(resultFromCache,result);
+        assertEquals(resultFromCache, result);
     }
 
+    @Test
+    void testCacheExpiry() throws InterruptedException {
+        int result = mathService.factorialOf(11);
+
+        Thread.sleep(12000);
+
+        int resultFromCache = mathService.factorialOf(11);
+        Mockito.verify(mathService, Mockito.times(2)).factorialOf(11);
+        assertEquals(resultFromCache, result);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Objects.requireNonNull(cacheManager.getCache("factorial")).clear();
+    }
 }
